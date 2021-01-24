a8cb0e1386f6a91f83cef4b44a4e4fbb7682e595
Crystal <18072638@brookes.ac.uk>
2021-01-24 15:24:59 +0000

    Changed get_loadtemp file name  to get_load, deleted old get_load file

--- final_year_project/repo/get_load.py
+++ final_year_project/repo/get_load.py
@@ -4,79 +4,111 @@ from interbotix_xs_modules.arm import InterbotixManipulatorXS
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Bool 
 import rospy
+import time 
 
 load_value_pub = rospy.Publisher( 'load_topic', Bool, queue_size = 0 )
-#joint_state_pub = rospy.Publisher( 'load_topic', Bool, queue_size = 0 )
 
 global cup_has_water
 global first_load_check
 global empty_cup_load
 global state
 global moment
+global jointLoad
 
+jointLoad = 0
 moment = 0
 state = "Start"
 empty_cup_load = 0
 first_load_check = True
 cup_has_water = False
 
+class State_machine:
+
+	def __init__(self):
+		self.state = "Start"
+		self.load = 0
+		self.empty_cup_load = 0
+		self.moment = 0
+		self.start_time = 0
+		self.timer_running = False
+	
+	def start_timer(self):
+		self.start_time = time.time()
+		self.timer_running = True
+	
+	def stop_timer(self):
+		self.timer_running = False
+	
+	def reset_timer(self):
+		self.start_time = time.time()
+		self.timer_running = True
+		
+	def time_elapsed(self):
+		elapsed = time.time() - self.start_time
+		print( "Time elapsed: " + str(elapsed) )
+		return elapsed
+		
+
 def set_wrist_pose():
 	print( "Moving arm into position..." )
 	neutral_joint_position = [0, 0, 0.506, -0.531, 0]
 	bot.arm.set_joint_positions( neutral_joint_position )
-	rospy.sleep( 2 )
+	rospy.sleep( 1 )
 	
 
 def listener():
 	rospy.Subscriber( "/rx150/joint_states", JointState, check_load )
 
 def check_load( joint_states ):
-	global state
-	global empty_cup_load
-	global moment 
+	global jointLoad
+	
+	jointLoad = joint_states.effort[3] 
+	#print("Load: " + str(jointLoad))
 
-	load = joint_states.effort[3] 
-	print("Load: " + str(load))
-	print("State: " + state)
+def process_state( State ):
+	print( "Process state load: " + str(State.load) )
+	print( "State: " + State.state )
 
-	if state == "Start":
-		empty_cup_load = empty_cup( load, empty_cup_load )	
-	elif state == "Waiting":
-		moment = filling_cup( load, empty_cup_load )
-	elif state == "Filling":
-		full_cup( load, moment )
-	elif state == "Ready":
+	if State.state == "Start":
+		State = empty_cup( State )	
+	elif State.state == "Waiting":
+		State = is_filling_cup( State )
+	elif State.state == "Filling":
+		State = cup_being_filled( State )
+	elif State.state == "Ready":
 		start_pouring()
-	return
+		State.state = "Finished"
+	return State
 
-def stabilise_load_timer():
-	
-
-def empty_cup( load, empty_cup_load ):
-	global state
-	if empty_cup_load == 0:
-		empty_cup_load = load
-		return empty_cup_load
+def empty_cup( robot_arm ):
+	if robot_arm.empty_cup_load == 0:
+		robot_arm.empty_cup_load = robot_arm.load
 	else:
-		empty_cup_load = load
-		state = "Waiting"
-		return empty_cup_load
+		robot_arm.empty_cup_load = robot_arm.load
+		robot_arm.state = "Waiting"
+	return robot_arm
 	
-def filling_cup( load, empty_cup_load ):
-	global state
-	global moment
+def is_filling_cup( robot_arm ):
+	if robot_arm.load != robot_arm.empty_cup_load:
+		robot_arm.moment = robot_arm.load
+		robot_arm.state = "Filling"
+		
+	return robot_arm
 
-	if load != empty_cup_load:
-		moment = load
-		state = "Filling"
-		return moment
-
-def full_cup( load, m ):
-	global state
-	if load != m:
-		state = "Filling"
+def cup_being_filled( robot_arm ):
+	if robot_arm.load != robot_arm.moment:
+		robot_arm.moment = robot_arm.load
+		if robot_arm.timer_running:
+			robot_arm.stop_timer()
 	else:
-		state = "Ready"
+		if robot_arm.timer_running:
+			if robot_arm.time_elapsed() > 5:
+				robot_arm.stop_timer()
+				robot_arm.state = "Ready"
+		else:
+			robot_arm.start_timer()	
+		
+	return robot_arm
 
 def start_pouring():
 	pouring_joint_positions = [0, 0, 0.506, -0.531, -1.57]
@@ -90,9 +122,12 @@ if __name__=='__main__':
 	bot = InterbotixManipulatorXS("rx150", "arm", "gripper")
 	set_wrist_pose()
 	listener()
-	while True:
+	robot_arm = State_machine()
+	while not rospy.is_shutdown():
 		
-		rospy.spin()
+		rospy.sleep( 0.05 )
+		robot_arm.load = jointLoad
+		robot_arm = process_state( robot_arm )
 
 
 
